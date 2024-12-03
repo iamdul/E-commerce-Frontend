72140ef4f934b325c44f75ceb59c1e9c330fd41b
iamdul <kalharidulanga@gmail.com>
2024-12-03 10:48:42 +0530

    create item amd popular section

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,39 @@
+import p1_img from './product1.jpg';
+import p2_img from './product2.jpg';
+import p3_img from './product3.jpg';
+import p4_img from './product4.jpg';
+
+
+const data_product = [
+    {
+      id: 1,
+      name: "Stripped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+      image: p1_img,
+      new_price: 50.00,
+      old_price: 80.50
+    },
+    {
+      id: 2,
+      name: "Floral Print Buttoned V-Neck Sleeveless Dress",
+      image: p2_img,
+      new_price: 45.00,
+      old_price: 70.00
+    },
+    {
+      id: 3,
+      name: "Casual Loose Fit Cotton Linen Blend Shirt",
+      image: p3_img,
+      new_price: 35.00,
+      old_price: 55.00
+    },
+    {
+      id: 4,
+      name: "High Waist Wide Leg Denim Jeans",
+      image: p4_img,
+      new_price: 60.00,
+      old_price: 90.00
+    }
+];
+
+  export default data_product;
+  
